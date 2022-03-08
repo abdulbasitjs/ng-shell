adab03eb77cd3d179bf5ba89ecf846e0059e1685
omer.durrani123 <omer.durrani@slashnext.net>
2022-03-08 11:54:30 +0500

    working on oti dashboard

--- src/app/modules/oti-dashboard/oti-dashboard.module.ts
+++ src/app/modules/oti-dashboard/oti-dashboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OtiDashboardRoutingModule } from './oti-dashboard-routing.module';
-import { OtiDashboardComponent } from './pages/oti-dashboard.component';
+import { OtiDashboardComponent } from './pages/oti-dashboard/oti-dashboard.component';
 
 
 @NgModule({
