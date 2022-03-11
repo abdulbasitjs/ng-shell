69aae39a483a3cfff0d05b4388b8479b3af337f0
omer.durrani123 <omer.durrani@slashnext.net>
2022-03-11 17:27:56 +0500

    fixes for seach, table ellipsis and font sizes

--- src/app/configs/ui/messages/index.ts
+++ src/app/configs/ui/messages/index.ts
@@ -1,10 +1,4 @@
 export const UIMESSAGES = {
-  INVALID_EMAIL: "We couldn't find an account with that email address.",
-  PROVIDE_EMAIL_USERNAME: 'Please enter a valid email.',
-  PROVIDE_EMAIL: 'Please enter a valid email.',
-  PROVIDE_FULL_NAME: 'Please enter full name.',
-  PROVIDE_PASSWORD: 'Please enter password.',
-  PASSWORD_NOT_MATCH: "Password did't match.",
   INVALID_EMAIL: "We couldn't find an account with that email address",
   PROVIDE_EMAIL_USERNAME: 'Please enter valid email/username',
   PROVIDE_EMAIL: 'Please enter valid email address',
